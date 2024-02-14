2a1669f682d60595adc05e1f9e0a12c15b6aaae7
Leosly <Leonardonigro11@gmail.com>
2024-02-14 08:21:36 -0500

    Switching working branch

---
+++ app/Petflix/Page.js
@@ -0,0 +1,17 @@
+"use client"
+import React from 'react';
+import NavBar from '../Menu';
+import ImageCarousel from '../slider.js';
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+
+const Petflix = () => {
+    const router = useRouter()
+  return (
+        <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center">
+    
+        </div>
+  );
+};
+
+export default Petflix;
