d500a5456c576017d3c3dfc0375b6e5b78d9831e
Leonardo <Leonardonigro11@gmail.com>
2025-07-04 14:50:37 -0400

    I hate JS

--- app/ResumeScanner/page.js
+++ app/ResumeScanner/page.js
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import { extractText, parseResumeSections, computeMatchPercentage } from '../utils/fileparsing.js';
+import { extractText, parseResumeSections, computeMatchPercentage } from '../utils/parseFiles.js';
 
 export default function ResumeScanner() {
   const [file, setFile] = useState(null);
