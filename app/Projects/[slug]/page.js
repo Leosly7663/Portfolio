d51ae741f643f5a2479b344a585788090b685fa4
Leonardo <Leonardonigro11@gmail.com>
2025-07-07 17:37:17 -0400

    app router?

--- app/Projects/[slug]/page.js
+++ app/Projects/[slug]/page.js
@@ -1,17 +1,20 @@
 import { useRouter } from "next/router";
 import ProjectTemplate from "../Components/ProjectTemplate";
-import projectsData from '../../data/projects';
+import projectsData from '@/data/projects';
 
-export async function getStaticPaths() {
-  const paths = projectsData.map((p) => ({ params: { slug: p.slug } }));
-  return { paths, fallback: false };
+export async function generateStaticParams() {
+  return projectsData.map((p) => ({
+    slug: p.slug,
+  }));
 }
 
-export async function getStaticProps({ params }) {
+export default function ProjectPage({ params }) {
   const project = projectsData.find((p) => p.slug === params.slug);
-  return { props: { project } };
-}
 
+  if (!project) return <div>Not found</div>;
+
+  return <ProjectTemplate {...project} />;
+}
 
 export default function ProjectPage({ project }) {
   const router = useRouter();
