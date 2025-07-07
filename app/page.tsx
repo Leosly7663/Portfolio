80b741ce9b7958e5cdaba57fb4f9276b0bb07063
Leonardo <Leonardonigro11@gmail.com>
2025-07-07 19:01:27 -0400

    new style

--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,27 @@
-"use client"
+"use client";
 import NavBar from './Components/Menu';
 import TitleCard from './Assets/Profile';
-import About from "./Assets/about"
+import About from "./Assets/about";
 import GradientTransition from "./Assets/backParticleMist";
 import ExperienceTimeline from "./Assets/experienceTimeline";
-import Projects from "./Assets/projects"
-import Footer from "./Assets/footer.js"
-import Fetch from "./Components/Fetch.js"
-import Highlight from "./Assets/highlights.js"
+import Projects from "./Assets/projects";
+import Footer from "./Assets/footer.js";
 
 const App = () => {
-
   return (
-    <div className="bg-slate-600">
-      <div className=" ">
-        <NavBar />
-      </div>
+    <div className="relative min-h-screen bg-slate-600 overflow-hidden">
+      {/* Background particles (auto full-screen + z-index) */}
+      <GradientTransition />
 
-      <div className='Main pt-10  bg-slate-600 '>
-        <div className="absolute justify-center flex-col w-full">
-          <div className=' bg-gradient-to-b from-gray-700 to-95% to-black absolute flex h-full inset-0 -z-10'>
-            <GradientTransition />
-          </div>
-          <TitleCard />
-          <div id="about">
-            <About />
-          </div>
-          <div id="highlights">
-            <Projects />
-          </div>
-           <div id="highlights">
-            <ExperienceTimeline />
-          </div>
-           
-          <div id="contact">
-            <Footer />
-          </div>
-
-        </div>
-
-
-
-
-
-      </div>
+      {/* Foreground content */}
+      <NavBar />
+      <main className="pt-10 relative z-10">
+        <TitleCard />
+        <div id="about"><About /></div>
+        <div id="highlights"><Projects /></div>
+        <div id="timeline"><ExperienceTimeline /></div>
+        <div id="contact"><Footer /></div>
+      </main>
     </div>
   );
 };
