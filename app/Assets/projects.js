5eb5b918bb88f0851bf19b77eb2326a826cc7578
Leosly <Leonardonigro11@gmail.com>
2025-07-12 14:36:17 -0400

    formaqtting

--- app/Assets/projects.js
+++ app/Assets/projects.js
@@ -17,18 +17,21 @@ const IconImage = ({ src, alt }) => (
 
 export default function ProjectsPage() {
   return (
-    <div className="space-y-8">
+    <div className="space-y-8 mt-20 p-20">
       {projectsData.map((p) => (
-        <div key={p.slug} className="bg-black bg-opacity-20 p-4 rounded-2xl">
-          <h2 className="text-xl font-semibold text-white">{p.title}</h2>
+        <div key={p.slug} className="bg-black bg-opacity-20 p-4 rounded-2xl flex-row flex">
+          <div className="flex flex-col">
+            <h2 className="text-xl font-semibold text-white">{p.title}</h2>
           <p className="text-gray-400 mb-2">{p.description}</p>
           <Link href={`/projects/${p.slug}`} className="text-blue-400 underline">
             View Project
           </Link>
-          {p.tech && (
-            <div className="mt-4 space-y-2">
+          </div>
+          <div className="flex flex-col items-center justify-center ml-4">
+            {p.tech && (
+            <div className="mt-4 space-y-2 p-4  flex flex-row">
               {p.tech.map((section, i) => (
-                <div key={i}>
+                <div key={i} className=" flex flex-row">
                   <h4 className="text-gray-300 mb-1">{section.title}:</h4>
                   <div className="flex space-x-2">
                     {section.items.map((img, index) => (
@@ -39,6 +42,7 @@ export default function ProjectsPage() {
               ))}
             </div>
           )}
+          </div>
         </div>
       ))}
     </div>
